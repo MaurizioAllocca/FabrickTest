623a34fbf456986077349e5ff533f64af4af002b
Maurizio Allocca <maurizio.allocca@ext.illimity.com>
2021-06-10 13:44:21 +0200

    chore: added JsonUtilsTest

--- src/main/java/it/fabrick/application/Application.java
+++ src/main/java/it/fabrick/application/Application.java
@@ -1,4 +1,4 @@
-package application;
+package it.fabrick.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/it/fabrick/utils/JsonUtilsTest.java
@@ -0,0 +1,59 @@
+package it.fabrick.utils;
+
+import it.fabrick.application.Application;
+import it.fabrick.entity.cashAccountTransactions.response.CashAccountTransactionsList;
+import it.fabrick.entity.cashAccountTransactions.response.CashAccountTransactionsPayload;
+import it.fabrick.entity.cashAccountTransactions.response.CashAccountTransactionsPayloadType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
+@SpringBootTest
+@ContextConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+public class JsonUtilsTest {
+
+    @Value("classpath:mocks/cash_account_transactions.json")
+    private Resource cashAccountTransactionsMock;
+
+    @Test
+    public void asPojoTest() throws IOException {
+        CashAccountTransactionsList cashAccountTransactionsList = JsonUtils.asPojo(
+            StreamUtils.copyToString
+                (cashAccountTransactionsMock.getInputStream(), Charset.defaultCharset())
+            , CashAccountTransactionsList.class);
+
+        assertThat(cashAccountTransactionsList, notNullValue(CashAccountTransactionsList.class));
+        assertThat(cashAccountTransactionsList.getList(), hasSize(11));
+        assertThat(cashAccountTransactionsList.getList(), hasItem(
+            CashAccountTransactionsPayload
+                .builder()
+                .transactionId("222260168301")
+                .operationId("21000094633763")
+                .accountingDate("2021-06-09")
+                .valueDate("2021-06-09")
+                .type(
+                    CashAccountTransactionsPayloadType
+                        .builder()
+                        .enumeration("GBS_TRANSACTION_TYPE")
+                        .value("GBS_ACCOUNT_TRANSACTION_TYPE_0009")
+                        .build())
+                .amount(-1.0)
+                .currency("EUR")
+                .description("BA JOHN DOE               PAYMENT INVOICE 75/2017")
+                .build()));
+    }
+}
