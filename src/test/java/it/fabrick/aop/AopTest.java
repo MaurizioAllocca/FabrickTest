add9b8eab8bdbce1a3f7da639785bd6c807093f3
Maurizio Allocca <maurizio.allocca@ext.illimity.com>
2021-06-10 16:27:05 +0200

    chore: added AopTest

---
+++ src/test/java/it/fabrick/aop/AopTest.java
@@ -0,0 +1,73 @@
+package it.fabrick.aop;
+
+import it.fabrick.application.Application;
+import it.fabrick.entity.cashAccountBalance.response.CashAccountBalancePayload;
+import it.fabrick.service.bankingaccount.IBankingAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ContextConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class AopTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IBankingAccountService bas;
+
+    @SpyBean
+    @Autowired
+    private Aop aop;
+
+    @Test
+    public void logControllerMethodEnteringTest() throws Exception {
+        when(bas.getCashAccountBalance())
+            .thenReturn(CashAccountBalancePayload
+                .builder()
+                .balance("80")
+                .build());
+
+        mockMvc.perform(
+            get("/fabrick/cashAccountBalance"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        verify(aop, times(1)).logControllerMethodEntering(any());
+    }
+
+    @Test
+    public void logControllerMethodExitingTest() throws Exception {
+        when(bas.getCashAccountBalance())
+            .thenReturn(CashAccountBalancePayload
+                .builder()
+                .balance("80")
+                .build());
+
+        mockMvc.perform(
+            get("/fabrick/cashAccountBalance"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        verify(aop, times(1)).logControllerMethodExiting(any());
+    }
+
+}
